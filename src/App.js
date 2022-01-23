49ef6318f648b98703db4ad46e54dcc90e0dd48c
Ujjwal-here <ujjwalsbx@gmail.com>
2022-01-23 10:09:50 +0530

    Bar Charts added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,227 @@
-import './App.css';
-import DatePicker from 'react-date-picker';
-import { useState } from 'react';
-import pattern from "./pattern"
+import "./App.css";
+import DatePicker from "react-date-picker";
+import { useState } from "react";
+import pattern from "./pattern";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function App() {
-  const [value,newValue]=useState(new Date("Sat May 01 2021 00:00:00 GMT+0530 (India Standard Time)"))
+  const [value, newValue] = useState(
+    new Date("Sat May 01 2021 00:00:00 GMT+0530 (India Standard Time)")
+  );
+  const [val, newVal] = useState(0);
 
-  const onChange=(date)=>{
-     newValue(date)
-  }
+  //On Change handler for datepicker
+  const onChange = (date) => {
+    newValue(date);
+  };
+
+  //This function converts this date and time(Sat May 01 2021 00:00:00 GMT+0530 (India Standard Time)) into simple date and time 
   function convert(str) {
-    var date = new Date(str),
+    let date = new Date(str),
       mnth = ("0" + (date.getMonth() + 1)).slice(-2),
       day = ("0" + date.getDate()).slice(-2);
     return [date.getFullYear(), mnth, day].join("-");
   }
-  const filtered=pattern.filter(person=>person.item_date === convert(value))
-  console.log(filtered)
 
-  function mapConversion(data){
-    const d={}
-    data.forEach(element => {
-      const dat=element.schedule_time.slice(0,10)
-      if (dat in d){
-          d[dat]+=1
-      }
-      else{
-          d[dat]=1
-      }
+  const filtered = pattern.filter(
+    (person) => person.item_date === convert(value)
+  );
+  console.log(filtered);
 
+  function mapConversion(data) {
+    const d = {};
+    data.forEach((element) => {
+      const dat = element.schedule_time.slice(0, 10);
+      if (dat in d) {
+        d[dat] += 1;
+      } else {
+        d[dat] = 1;
+      }
     });
-    return d
+    return d;
   }
-  console.log(mapConversion(filtered))
+  const d = mapConversion(filtered);
+
+  //Labels and values for first graph
+  const labels = [];
+  const vals = [];
+  for (let key in d) {
+    labels.push(key);
+    vals.push(d[key]);
+  }
+
+  //data for first graph
+  const states = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Schedule Count on Particular Date",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+
+        data: vals,
+      },
+    ],
+  };
+  const h = {
+    "9AM to 12AM": 0,
+    "12AM to 3PM": 0,
+    "3PM to 6PM": 0,
+    "6PM to 9PM": 0,
+    "9PM to 12AM": 0,
+    "12AM to 3AM": 0,
+    "3AM to 6AM": 0,
+    "6AM to 9AM": 0,
+  };
+
+  //
+  const filter = filtered.filter(
+    (ele) => ele.schedule_time.slice(0, 10) === Object.keys(d)[val]
+  );
+  console.log(filter);
   
+  //Filtering data based on click event
+  filter.forEach((elem) => {
+    const time = parseInt(elem.schedule_time.slice(11, 14));
+
+    const hours = ((time + 11) % 12) + 1;
+
+    const suffix = time >= 12 ? "pm" : "am";
+    console.log(hours, suffix);
+
+    if (suffix === "pm" && hours >= 6 && hours < 9) {
+      h["6PM to 9PM"] += 1;
+    } else if (suffix === "pm" && hours >= 12 && hours < 15) {
+      h["12AM to 3PM"] += 1;
+    } else if (suffix === "pm" && hours >= 3 && hours < 6) {
+      h["3PM to 6PM"] += 1;
+    } else if (suffix === "am" && hours >= 12 && hours < 15) {
+      h["12AM to 3AM"] += 1;
+    } else if (suffix === "am" && hours >= 3 && hours < 6) {
+      h["3AM to 6AM"] += 1;
+    } else if (suffix === "am" && hours >= 9 && hours < 12) {
+      h["9AM to 12AM"] += 1;
+    } else if (suffix === "pm" && hours >= 9 && hours < 12) {
+      h["9PM to 12AM"] += 1;
+    } else if (suffix === "am" && hours >= 6 && hours < 9) {
+      h["6AM to 9AM"] += 1;
+    }
+  });
+
+  console.log(h);
+
+  //Labels and values for second bar
+  const lab = [];
+  const values = [];
+  for (let key in h) {
+    lab.push(key);
+    values.push(h[key]);
+  }
+
+  // Data For Second Bar
+  const stat = {
+    labels: lab,
+    datasets: [
+      {
+        label: "Schedule Count on Particular Date",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+
+        data: values,
+      },
+    ],
+  };
   return (
     <div className="App">
-      <DatePicker onChange={onChange} value={value} format='y-MM-dd'/>
+      <DatePicker onChange={onChange} value={value} format="y-MM-dd" />
+      <Bar
+        className="bar"
+        data={states}
+        options={{
+          onHover:function(e){
+            e.native.target.style.cursor = 'pointer';
+          },
+          onClick: function (c, i) {
+            newVal(i[0].index);
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: "Customer Schedulling Pattern",
+            },
+            legend: {
+              display: true,
+              position: "bottom",
+            },
+          },
+        }}
+      />
+      <Bar
+        className="bar"
+        data={stat}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Customer Schedulling Pattern",
+            },
+            legend: {
+              display: true,
+              position: "bottom",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
